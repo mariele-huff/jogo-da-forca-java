c4d61269b5f25dd2606bcc000e16c149088d35e6
unknown <mariele.huff@universo.univates.br>
2024-05-07 08:37:05 -0300

    Implementação simples de jogo da forca

---
+++ src/Objeto.java
@@ -0,0 +1,22 @@
+
+/**
+ *
+ * @author Mariele
+ */
+public class Objeto {
+    private final String nome;
+    private final String dica;
+
+    public Objeto(String nome, String dica) {
+        this.nome = nome;
+        this.dica = dica;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDica() {
+        return dica;
+    }
+}
